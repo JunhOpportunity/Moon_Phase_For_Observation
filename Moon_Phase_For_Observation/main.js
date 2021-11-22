b023dc7862880359074d08120be7015c0a90786a
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-11-23 01:13:47 +0900

    By Developer Pictures CSS Style ADD

--- Moon_Phase_For_Observation/main.js
+++ Moon_Phase_For_Observation/main.js
@@ -6,3 +6,9 @@
 //     document.body.style.backgroundColor = "tomato";
 // }
 // btn.addEventListener("click", bgchange);
+
+const do_not_right_click = document.querySelector('img');
+function right_click() {
+  alert("무단 배포와 도용을 금지합니다.");
+}
+do_not_right_click.eventlistener('click', right_click);

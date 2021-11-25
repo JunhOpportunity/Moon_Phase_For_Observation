16e2ed76138ded337bf19e04121726b9323f8f79
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-11-25 23:24:59 +0900

    Floating CSS Style ADD

--- Moon_Phase_For_Observation/main.js
+++ Moon_Phase_For_Observation/main.js
@@ -12,3 +12,6 @@ function right_click() {
   alert("무단 배포와 도용을 금지합니다.");
 }
 do_not_right_click.eventlistener('click', right_click);
+
+// Home 버튼 누를 경우 맨 위로 천천히 이동
+const scrollHome = document.querySelector()

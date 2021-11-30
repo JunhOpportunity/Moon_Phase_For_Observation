d0b077b1376e006a6e17e598ddbf855253205951
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-11-30 23:57:53 +0900

    Next Full Moon Day Info & Quiz ADD + Style Modify

--- Moon_Phase_For_Observation/main.js
+++ Moon_Phase_For_Observation/main.js
@@ -7,11 +7,22 @@
 // }
 // btn.addEventListener("click", bgchange);
 
-const do_not_right_click = document.querySelector('img');
-function right_click() {
-  alert("무단 배포와 도용을 금지합니다.");
-}
-do_not_right_click.eventlistener('click', right_click);
+// const do_not_right_click = document.querySelector('img');
+// function right_click() {
+//   alert("무단 배포와 도용을 금지합니다.");
+// }
+// do_not_right_click.eventlistener('click', right_click);
 
 // Home 버튼 누를 경우 맨 위로 천천히 이동
-const scrollHome = document.querySelector()
+// const scrollHome = document.querySelector()
+
+
+
+// 이미지를 무단으로 퍼가려고 할 경우
+function windowCopy() {
+  alert("이미지를 복사하실 수 없습니다.");
+};
+window.addEventListener("copy", windowCopy);
+// let img = document.querySelector('#developerPictures');
+// console.log(img);
+// img_hover.addEventListener("click", windowCopy);

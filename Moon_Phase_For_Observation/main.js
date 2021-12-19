66da95c832bbe6c307301644eaf42befbe1ad7e7
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-12-19 23:52:19 +0900

    JSON & JQUERY Tomorrow Plan

--- Moon_Phase_For_Observation/main.js
+++ Moon_Phase_For_Observation/main.js
@@ -32,25 +32,37 @@ body.addEventListener("copy", copyTry);
 // Today Observation Moon Quiz
 function moonQuiz() {
   let todayWeather = True; // 임시 저장
+  let btnclick;
+  let yesBtn;
+  let noBtn;
+
   if (btnclick === yesBtn) { // 만약 Yes 를 누를 경우
     if(todayWeather === True) { // 관측이 가능 할 경우
-      // 색깔 바뀜 -> alert로 '맞습니다! 오늘은 관측이 가능합니다!' 출력
+      // toggle로 색깔 바뀜 -> alert로 '맞습니다! 오늘은 관측이 가능합니다!' 출력
       alert('맞습니다! 오늘은 관측이 가능합니다!');
     }
     else { // 관측이 불가능 할 경우
-      // 색깔 바뀜 -> alert로 '틀렸습니다! 아쉽지만 오늘은 관측이 불가능 한 날입니다..' 출력
+      // toggle로 색깔 바뀜 -> alert로 '틀렸습니다! 아쉽지만 오늘은 관측이 불가능 한 날입니다..' 출력
       alert('틀렸습니다! 아쉽지만 오늘은 관측이 불가능 한 날입니다..');
     }
   }
   else if (btnclick === noBtn) { // 만약 No 를 누를 경우
     if(todayWeather === True) { // 관측이 가능 할 경우
-      // 색깔 바뀜 -> alert로 '틀렸습니다! 오늘은 관측이 가능합니다!' 출력
+      // toggle로 색깔 바뀜 -> alert로 '틀렸습니다! 오늘은 관측이 가능합니다!' 출력
       alert('틀렸습니다! 오늘은 관측이 가능합니다!');
     }
     else { // 관측이 불가능 할 경우
-      // 색깔 바뀜 -> alert로 '맞습니다! 아쉽지만 오늘은 관측이 불가능 한 날입니다..' 출력
+      // toggle로 색깔 바뀜 -> alert로 '맞습니다! 아쉽지만 오늘은 관측이 불가능 한 날입니다..' 출력
       alert('맞습니다! 아쉽지만 오늘은 관측이 불가능 한 날입니다..');
     };
 };
 
+// https://www.youtube.com/watch?v=ccTAedW2KPg&list=WL&index=2 
+// 30:00 부터.
+// jquery 공부 -> json 파일 가져오는 방법 w3school 에서 든지 아니면 다른 강의 든지
 
+/* JSON 데이터 가져오는 문법
+  $.getJSON('파일경로', function(data){ // 파일 경로는 도시 ID와 KEY를 가지고 만든 주소
+      // data로 할 일 작성
+  });
+*/ 
